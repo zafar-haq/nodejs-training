383afaeb055ff50d343692f060dc2e8d62e506d1
Zafar Ul Haq <zafar.haq@invozone.com>
2021-12-14 21:00:07 +0500

    async , await, callback

--- app.mjs
+++ app.mjs
@@ -6,3 +6,14 @@ import { log, checkHelloWorld, p_module } from './my_modules/practice.mjs';
 log("hello");
 checkHelloWorld('Hello World');
 p_module.getDate();
+p_module.getUser()
+    .then((user) => {
+        console.log(user);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+p_module.myDisplay().then((value) => {
+    console.log(value);
+});
--- my_modules/practice_module.mjs
+++ my_modules/practice_module.mjs
@@ -3,3 +3,31 @@
 export var getDate = function(){
     console.log(new Date());
 }
+
+const readUser = function(){
+    return {name:'zafar'};
+}
+
+//promises
+
+export const getUser = function(){
+
+    return new Promise((resolve, reject) => {
+        const user = readUser();
+        if(user !== null && user !== NaN){
+            resolve(user);
+        }else{
+            reject('user was not found.');
+        }
+    });
+}
+
+//async and await
+
+export async function myDisplay() {
+    let myPromise = new Promise(function(resolve) {
+      setTimeout(function() {resolve("hello");}, 2000);
+    });
+    console.log(await myPromise);
+    return "how are you?";
+  }
